2d9ec3e88afaaf01231816f93ec54c79eb0d7a11
ErrazIG <frederic-vml@hotmail.fr>
2024-06-11 16:33:40 +0200

    create contact partner table

---
+++ models/contactPartner.model.js
@@ -0,0 +1,36 @@
+"use strict";
+
+import { DataTypes, Sequelize } from "sequelize";
+
+/** contactPartner model
+ * @param {Sequelize} sequelize
+ * @returns
+ */
+export default (sequelize) => {
+  const ContactPartner = sequelize.define(
+    "contactPartner",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+  return ContactPartner;
+};

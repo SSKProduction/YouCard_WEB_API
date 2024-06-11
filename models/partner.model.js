c6aa048550e3b6958f3d903ac41265ce042e6e02
ErrazIG <frederic-vml@hotmail.fr>
2024-06-11 16:33:17 +0200

    create partner table

---
+++ models/partner.model.js
@@ -0,0 +1,68 @@
+"use strict";
+
+import { DataTypes, Sequelize } from "sequelize";
+
+/** partner model
+ * @param {Sequelize} sequelize
+ * @returns
+ */
+export default (sequelize) => {
+  const Partner = sequelize.define(
+    "partner",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      address_country: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      address_city: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      address_street: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      address_street_number: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      address_postcode: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      image_profile: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      contact_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+  return Partner;
+};

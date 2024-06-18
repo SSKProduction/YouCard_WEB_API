521c71f18a3bd0712886661e04536efcea0a73fd
ErrazIG <frederic-vml@hotmail.fr>
2024-06-18 16:53:50 +0200

    fix generateJwt

--- utils/jwt.utils.js
+++ utils/jwt.utils.js
@@ -1,26 +1,24 @@
 "use strict";
 import jwt from "jsonwebtoken";
 
-export const generateJwt = ({ id, username, role_id, img }) => {
-  return new Promise((resolve, reject) => {
-    const data = { id, username, role_id, img };
-    const secret = process.env.JWT_SECRET;
-    const options = {
-      algorithm: "HS512",
-      expiresIn: "12h",
-      audience: process.env.JWT_AUDIENCE,
-      issuer: process.env.JWT_ISSUER,
-    };
+export const generateJwt = ({
+  id,
+  firstname,
+  lastname,
+  email,
+  role_id,
+  subscription_id,
+}) => {
+  const data = { id, firstname, lastname, email, role_id, subscription_id };
+  const secret = process.env.JWT_SECRET;
+  const options = {
+    algorithm: "HS512",
+    expiresIn: "12h",
+    audience: process.env.JWT_AUDIENCE,
+    issuer: process.env.JWT_ISSUER,
+  };
 
-    jwt.sign(data, secret, options, (error, token) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-
-      resolve(token);
-    });
-  });
+  return jwt.sign(data, secret, options);
 };
 
 export const decodeJwt = (token) => {

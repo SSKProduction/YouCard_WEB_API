cb2cecee796024bfb874d7db14b73dd01c36e59e
ErrazIG <frederic-vml@hotmail.fr>
2024-06-18 16:54:58 +0200

    registerMember + registerPartner

--- services/auth.service.js
+++ services/auth.service.js
@@ -2,7 +2,7 @@ import argon2 from "argon2";
 import db from "../models/index.js";
 
 const authService = {
-  register: async (email, password) => {
+  registerMember: async (firstname, lastname, email, password) => {
     try {
       // Vérifiez si l'email est déjà utilisé
       const existingMember = await db.Member.findOne({ where: { email } });
@@ -15,6 +15,8 @@ const authService = {
 
       // Créez un nouveau membre
       const newMember = await db.Member.create({
+        firstname,
+        lastname,
         email,
         password: hashedPassword,
         role_id: 1,
@@ -22,6 +24,30 @@ const authService = {
       });
 
       return newMember;
+    } catch (error) {
+      throw error;
+    }
+  },
+  registerPartner: async (email, password) => {
+    try {
+      // Vérifiez si l'email est déjà utilisé
+      const existingPartner = await db.Partner.findOne({ where: { email } });
+      if (existingPartner) {
+        throw new Error("L'email est déjà utilisé.");
+      }
+
+      // Hash le mot de passe
+      const hashedPassword = await argon2.hash(password);
+
+      // Créez un nouveau membre
+      const newPartner = await db.Partner.create({
+        email,
+        password: hashedPassword,
+        role_id: 1,
+        subscription_id: 1,
+      });
+
+      return newPartner;
     } catch (error) {
       throw error;
     }

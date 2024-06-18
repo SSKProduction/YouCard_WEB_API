9e44d78d6e7c00dd3f32ca8d1890663600d9cb5b
ErrazIG <frederic-vml@hotmail.fr>
2024-06-18 17:40:26 +0200

    add member router

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import authRouter from "./auth.router.js";
+import memberRouter from "./member.router.js";
 const mainRouter = Router();
 
 mainRouter.use("/auth", authRouter);
+mainRouter.use("/member", memberRouter);
 
 export default mainRouter;

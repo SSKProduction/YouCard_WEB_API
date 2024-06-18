9f629bdc7a6cad360238db7c8722bc30e4e56e5c
ErrazIG <frederic-vml@hotmail.fr>
2024-06-18 17:40:45 +0200

    add partner dto

---
+++ DTO/partnerDTO.js
@@ -0,0 +1,10 @@
+export class PartnerDTO {
+  constructor({ id, firstname, lastname, email, phone, url_website }) {
+    this.id = id;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = email;
+    this.phone = phone;
+    this.url_website = url_website;
+  }
+}

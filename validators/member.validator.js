ef245f949867d1641319baffce85a3326620b294
ErrazIG <frederic-vml@hotmail.fr>
2024-06-20 15:45:13 +0200

    fix validator member

--- validators/member.validator.js
+++ validators/member.validator.js
@@ -11,36 +11,17 @@ export const memberUpdateValidator = yup.object().shape({
     .string()
     .typeError("Le nom est invalide")
     .required("Le nom est obligatoire"),
-  birthdate: yup
-    .date()
-    .typeError("La date de naissance est invalide")
-    .required("La date de naissance est obligatoire"),
-  address_country: yup
-    .string()
-    .typeError("Le pays est invalide")
-    .required("Le pays est obligatoire"),
-  address_city: yup
-    .string()
-    .typeError("La ville est invalide")
-    .required("Le ville est obligatoire"),
-  address_street: yup
-    .string()
-    .typeError("La rue est invalide")
-    .required("La rue est obligatoire"),
+  birthdate: yup.date().typeError("La date de naissance est invalide"),
+  address_country: yup.string().typeError("Le pays est invalide"),
+  address_city: yup.string().typeError("La ville est invalide"),
+  address_street: yup.string().typeError("La rue est invalide"),
   address_street_number: yup
     .string()
-    .typeError("Le numero de rue est invalide")
-    .required("Le numero de rue est obligatoire"),
-  address_postcode: yup
-    .string()
-    .typeError("Le code postale rue est invalide")
-    .required("Le code postale est obligatoire"),
+    .typeError("Le numero de rue est invalide"),
+  address_postcode: yup.string().typeError("Le code postale rue est invalide"),
   email: yup
     .string()
     .typeError("L'email est invalide")
     .required("L'email est obligatoire"),
-  role_id: yup
-    .string()
-    .typeError("Le role est invalide")
-    .required("Le role est obligatoire"),
+  role_id: yup.string().typeError("Le role est invalide"),
 });

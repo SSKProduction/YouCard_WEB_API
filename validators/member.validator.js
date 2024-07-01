70a922d1997f9c704e49ecc18643616ce88a621d
ErrazIG <frederic-vml@hotmail.fr>
2024-07-01 13:10:06 +0200

    update password validator

--- validators/member.validator.js
+++ validators/member.validator.js
@@ -25,3 +25,23 @@ export const memberUpdateValidator = yup.object().shape({
     .required("L'email est obligatoire"),
   role_id: yup.string().typeError("Le role est invalide"),
 });
+
+export const memberUpdatePwdValidator = yup.object().shape({
+  currentPassword: yup
+    .string()
+    .typeError("Le mot de passe est invalide")
+    .required("Le mot de passe actuel est obligatoire"),
+  password: yup
+    .string()
+    .typeError("Le mot de passe est invalide")
+    .matches(
+      pwdRegex,
+      "Votre mot de passe doit contenir au moins 8 caractères, incluant une majuscule, une minuscule, un chiffre et un caractère spécial."
+    )
+    .required("Le mot de passe est obligatoire"),
+  confirmPassword: yup
+    .string()
+    .typeError("Le pseudo est invalide")
+    .required("Le mot de passe de confirmation est obligatoire")
+    .oneOf([yup.ref("password")], "Les mots de passe doivent correspondre"),
+});

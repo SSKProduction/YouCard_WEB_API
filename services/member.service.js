c7d08697d88f840338aea1ee149ee2f1a019c3fc
ErrazIG <frederic-vml@hotmail.fr>
2024-07-01 13:09:30 +0200

    update password service

--- services/member.service.js
+++ services/member.service.js
@@ -1,3 +1,4 @@
+import argon2 from "argon2";
 import { MemberDTO } from "../DTO/memberDTO.js";
 import db from "../models/index.js";
 
@@ -46,6 +47,36 @@ const memberService = {
       await member.update(filteredData, { transaction });
       await transaction.commit();
       return !!member ? new MemberDTO(member) : null;
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  },
+  verifyCurrentPassword: async (id, currentPassword) => {
+    const member = await db.Member.findOne({ where: { id } });
+
+    if (!member) {
+      throw new Error("L'utilisateur est introuvable.");
+    }
+
+    const isMatch = await argon2.verify(member.password, currentPassword);
+    return isMatch;
+  },
+  updatePassword: async (id, newPwd) => {
+    const transaction = await db.sequelize.transaction();
+
+    try {
+      const member = await db.Member.findOne({ where: { id } });
+
+      if (!member) {
+        await transaction.rollback();
+        return null;
+      }
+      const newHashPwd = await argon2.hash(newPwd);
+
+      await member.update({ password: newHashPwd }, { transaction });
+      await transaction.commit();
+      return true;
     } catch (error) {
       await transaction.rollback();
       throw error;

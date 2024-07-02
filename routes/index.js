f78a37b4ef5bcc29fd6eea18c44d1df1e2befe7f
ErrazIG <frederic-vml@hotmail.fr>
2024-07-02 14:58:50 +0200

    add partner router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRouter from "./auth.router.js";
 import memberRouter from "./member.router.js";
+import partnerRouter from "./partner.router.js";
 const mainRouter = Router();
 
 mainRouter.use("/auth", authRouter);
 mainRouter.use("/member", memberRouter);
+mainRouter.use("/partner", partnerRouter);
 
 export default mainRouter;

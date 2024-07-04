6cb3c0196720dc7d698f992253d4b01596b61094
ErrazIG <frederic-vml@hotmail.fr>
2024-07-04 14:08:15 +0200

    register partner service

--- services/auth.service.js
+++ services/auth.service.js
@@ -17,17 +17,18 @@ const authService = {
     address_postcode
   ) => {
     try {
-      // Vérifiez si l'email est déjà utilisé
+      // vérifier si l'email existe déjà dans la base de données
       const existingMember = await db.Member.findOne({ where: { email } });
       if (existingMember) {
+        // si l'email est déjà utilisé, lancer une erreur
         throw new Error("L'email est déjà utilisé.");
       }
       console.log("LE PASSWORD", password);
 
-      // Hash le mot de passe
+      // hacher le mot de passe avant de le stocker
       const hashedPassword = await argon2.hash(password);
 
-      // Créez un nouveau membre
+      // créer un nouveau membre dans la base de données
       const newMember = await db.Member.create({
         firstname,
         lastname,
@@ -39,52 +40,60 @@ const authService = {
         address_street,
         address_street_number,
         address_postcode,
-        role_id: 1,
-        subscription_id: 1,
+        role_id: 1, // rôle par défaut pour les membres
+        subscription_id: 1, // abonnement par défaut
       });
 
+      // retourner le DTO du nouveau membre
       return new MemberDTO(newMember);
     } catch (error) {
+      // en cas d'erreur, la relancer
       throw error;
     }
   },
-  registerPartner: async (
-    name,
-    email,
-    password,
-    address_country,
-    address_city,
-    address_street,
-    address_street_number,
-    address_postcode,
-    contact_id
-  ) => {
+
+  registerPartner: async (partnerData, contactData) => {
+    // démarrer une transaction
+    const t = await db.sequelize.transaction();
     try {
-      // Vérifiez si l'email est déjà utilisé
-      const existingPartner = await db.Partner.findOne({ where: { email } });
+      // vérifier si l'email du partenaire existe déjà
+      const existingPartner = await db.Partner.findOne(
+        {
+          where: { email_partner: partnerData.email_partner },
+        },
+        { transaction: t }
+      );
       if (existingPartner) {
-        throw new Error("L'email est déjà utilisé.");
+        // si l'email est déjà utilisé, lancer une erreur
+        throw new Error("L'email du partenaire est déjà utilisé.");
       }
-      //! continuer avec le contact_id !!!!!!!!!!!!!!
 
-      // Hash le mot de passe
-      const hashedPassword = await argon2.hash(password);
-
-      // Créez un nouveau membre
-      const newPartner = await db.Partner.create({
-        name,
-        email,
-        password: hashedPassword,
-        address_country,
-        address_city,
-        address_street,
-        address_street_number,
-        address_postcode,
-        contact_id,
+      // créer un nouveau contact pour le partenaire
+      const newContact = await db.ContactPartner.create(contactData, {
+        transaction: t,
       });
+      console.log(newContact);
 
+      // hacher le mot de passe du partenaire avant de le stocker
+      const hashedPassword = await argon2.hash(partnerData.password);
+
+      // créer un nouveau partenaire dans la base de données
+      const newPartner = await db.Partner.create(
+        {
+          ...partnerData,
+          password: hashedPassword,
+          contact_id: newContact.id, // lier le contact créé au partenaire
+        },
+        { transaction: t }
+      );
+
+      // valider la transaction
+      await t.commit();
+      // retourner le DTO du nouveau partenaire
       return new PartnerDTO(newPartner);
     } catch (error) {
+      // en cas d'erreur, annuler la transaction et la relancer
+      await t.rollback();
       throw error;
     }
   },

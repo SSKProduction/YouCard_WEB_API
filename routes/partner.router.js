ec99b1584629b0a0f5145594e5d7763a1e124164
ErrazIG <frederic-vml@hotmail.fr>
2024-07-04 14:29:38 +0200

    ajout route update

--- routes/partner.router.js
+++ routes/partner.router.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import partnerController from "../controllers/partner.controller.js";
+import { bodyValidatorMiddleware } from "../middlewares/body-validator.middleware.js";
+import { partnerUpdate } from "../validators/partner.validator.js";
 
 const partnerRouter = Router();
 
@@ -10,5 +12,6 @@ partnerRouter
 partnerRouter
   .route("/:id")
   .get(partnerController.getOne)
+  .patch(bodyValidatorMiddleware(partnerUpdate), partnerController.update)
   .all((_, res) => res.sendStatus(405));
 export default partnerRouter;

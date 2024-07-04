7c23a76493f76c98da5c9cf9197bcbffbdf45eaa
ErrazIG <frederic-vml@hotmail.fr>
2024-07-04 14:06:55 +0200

    contact partner dto

---
+++ DTO/contactPartnerDTO.js
@@ -0,0 +1,9 @@
+export class contactPartnerDTO {
+  constructor({ id, firstname, lastname, email, phone }) {
+    this.id = id;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = email;
+    this.phone = phone;
+  }
+}

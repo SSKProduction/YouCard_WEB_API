2de5dd696d509a84c59dc075287d01f7a590dc3b
ErrazIG <frederic-vml@hotmail.fr>
2024-07-04 14:06:42 +0200

    partner dto

--- DTO/partnerDTO.js
+++ DTO/partnerDTO.js
@@ -1,10 +1,19 @@
 export class PartnerDTO {
-  constructor({ id, firstname, lastname, email, phone, url_website }) {
+  constructor({
+    id,
+    name,
+    email_partner,
+    phone,
+    image_profile,
+    url_website,
+    contact_id,
+  }) {
     this.id = id;
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.email = email;
+    this.name = name;
+    this.email_partner = email_partner;
     this.phone = phone;
+    this.image_profile = image_profile;
     this.url_website = url_website;
+    this.contact_id = contact_id;
   }
 }

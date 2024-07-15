721c982c2ebd8f2eedc17a8db34115a15386bad6
ErrazIG <frederic-vml@hotmail.fr>
2024-07-15 14:59:24 +0200

    contact partner service methode update get et get all

--- services/contactPartner.service.js
+++ services/contactPartner.service.js
@@ -14,6 +14,43 @@ const contactPartner = {
     const contactPartner = await db.ContactPartner.create(contactPartnerData);
     return new contactPartnerDTO(contactPartner);
   },
+  update: async (id, updateData) => {
+    const transaction = await db.sequelize.transaction();
+
+    try {
+      const contactPartner = await db.ContactPartner.findOne({
+        where: { id },
+        transaction,
+      });
+
+      if (!contactPartner) {
+        throw new Error("La personne de contact est introuvable.");
+      }
+
+      await contactPartner.update(updateData, { transaction });
+      await transaction.commit();
+
+      return new contactPartnerDTO(contactPartner);
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  },
+  get: async (id) => {
+    const contactPartner = await db.ContactPartner.findOne({ where: { id } });
+
+    if (!contactPartner) {
+      return null;
+    }
+
+    return new contactPartnerDTO(contactPartner);
+  },
+  getAll: async () => {
+    const contactPartners = await db.ContactPartner.findAll();
+    return contactPartners.map(
+      (contactPartner) => new contactPartnerDTO(contactPartner)
+    );
+  },
   delete: async (id) => {
     const transaction = await db.sequelize.transaction();
     try {
